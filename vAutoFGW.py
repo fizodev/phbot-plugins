33834ff54219dc99235cdcc53faa339f99394deb
Virus <m.abdelhafiz94@gmail.com>
2026-07-10 18:55:07 +0300

    refactor: clean up vAutoFGW.py to keep only FGW Dimension entry

--- vAutoFGW.py
+++ vAutoFGW.py
@@ -2,331 +2,23 @@ from phBot import *
 import QtBind
 from threading import Timer
 from time import sleep
-import sqlite3
-import json
 import struct
-import os
 
 pVersion = '1.0.0'
 pName = 'vAutoFGW'
 
-# ______________________________ Initializing ______________________________ #
-
 DIMENSIONAL_COOLDOWN_DELAY = 18000 # seconds (5 hours)
-WAIT_DROPS_DELAY_MAX = 10 # seconds
-COUNT_MOBS_DELAY = 1.0 # seconds
 
 # Globals
-character_data = None
 itemUsedByPlugin = None
 dimensionalItemActivated = None
 
 # Graphic user interface
-gui = QtBind.init(__name__,pName)
-
-lblMobs = QtBind.createLabel(gui,'#   Add monster names to ignore    #\n#          from Monster Counter         #',31,6)
-tbxMobs = QtBind.createLineEdit(gui,"",31,35,100,20)
-lstMobs = QtBind.createList(gui,31,56,176,203)
-lstMobsData = []
-btnAddMob = QtBind.createButton(gui,'btnAddMob_clicked',"    Add    ",132,34)
-btnRemMob = QtBind.createButton(gui,'btnRemMob_clicked',"     Remove     ",80,258)
-
-lblMonsterCounter = QtBind.createLabel(gui,"#                 Monster Counter                 #",520,6)
-lstMonsterCounter = QtBind.createList(gui,520,23,197,237)
-QtBind.append(gui,lstMonsterCounter,'Name (Type)') # Header
-
-lblPreferences = QtBind.createLabel(gui,"#             Monster Counter preferences            #",240,6)
-lstIgnore = []
-lstOnlyCount = []
-
-_y = 26
-lblGeneral = QtBind.createLabel(gui,'General (0)',240,_y)
-cbxIgnoreGeneral = QtBind.createCheckBox(gui,'cbxIgnoreGeneral_clicked','Ignore',345,_y)
-cbxOnlyCountGeneral = QtBind.createCheckBox(gui,'cbxOnlyCountGeneral_clicked','Only Count',405,_y)
-_y+=20
-lblChampion = QtBind.createLabel(gui,'Champion (1)',240,_y)
-cbxIgnoreChampion = QtBind.createCheckBox(gui,'cbxIgnoreChampion_clicked','Ignore',345,_y)
-cbxOnlyCountChampion = QtBind.createCheckBox(gui,'cbxOnlyCountChampion_clicked','Only Count',405,_y)
-_y+=20
-lblGiant = QtBind.createLabel(gui,'Giant (4)',240,_y)
-cbxIgnoreGiant = QtBind.createCheckBox(gui,'cbxIgnoreGiant_clicked','Ignore',345,_y)
-cbxOnlyCountGiant = QtBind.createCheckBox(gui,'cbxOnlyCountGiant_clicked','Only Count',405,_y)
-_y+=20
-lblTitan = QtBind.createLabel(gui,'Titan (5)',240,_y)
-cbxIgnoreTitan = QtBind.createCheckBox(gui,'cbxIgnoreTitan_clicked','Ignore',345,_y)
-cbxOnlyCountTitan = QtBind.createCheckBox(gui,'cbxOnlyCountTitan_clicked','Only Count',405,_y)
-_y+=20
-lblStrong = QtBind.createLabel(gui,'Strong (6)',240,_y)
-cbxIgnoreStrong = QtBind.createCheckBox(gui,'cbxIgnoreStrong_clicked','Ignore',345,_y)
-cbxOnlyCountStrong = QtBind.createCheckBox(gui,'cbxOnlyCountStrong_clicked','Only Count',405,_y)
-_y+=20
-lblElite = QtBind.createLabel(gui,'Elite (7)',240,_y)
-cbxIgnoreElite = QtBind.createCheckBox(gui,'cbxIgnoreElite_clicked','Ignore',345,_y)
-cbxOnlyCountElite = QtBind.createCheckBox(gui,'cbxOnlyCountElite_clicked','Only Count',405,_y)
-_y+=20
-lblUnique = QtBind.createLabel(gui,'Unique (8)',240,_y)
-cbxIgnoreUnique = QtBind.createCheckBox(gui,'cbxIgnoreUnique_clicked','Ignore',345,_y)
-cbxOnlyCountUnique = QtBind.createCheckBox(gui,'cbxOnlyCountUnique_clicked','Only Count',405,_y)
-_y+=20
-lblParty = QtBind.createLabel(gui,'Party (16)',240,_y)
-cbxIgnoreParty = QtBind.createCheckBox(gui,'cbxIgnoreParty_clicked','Ignore',345,_y)
-cbxOnlyCountParty = QtBind.createCheckBox(gui,'cbxOnlyCountParty_clicked','Only Count',405,_y)
-_y+=20
-lblChampionParty = QtBind.createLabel(gui,'ChampionParty (17)',240,_y)
-cbxIgnoreChampionParty = QtBind.createCheckBox(gui,'cbxIgnoreChampionParty_clicked','Ignore',345,_y)
-cbxOnlyCountChampionParty = QtBind.createCheckBox(gui,'cbxOnlyCountChampionParty_clicked','Only Count',405,_y)
-_y+=20
-lblGiantParty = QtBind.createLabel(gui,'GiantParty (20)',240,_y)
-cbxIgnoreGiantParty = QtBind.createCheckBox(gui,'cbxIgnoreGiantParty_clicked','Ignore',345,_y)
-cbxOnlyCountGiantParty = QtBind.createCheckBox(gui,'cbxOnlyCountGiantParty_clicked','Only Count',405,_y)
-
-_y+=30
-cbxAcceptForgottenWorld = QtBind.createCheckBox(gui,'cbxAcceptForgottenWorld_checked','Accept Forgotten World invitations',240,_y)
-
-# ______________________________ Methods ______________________________ #
-
-def cbxIgnoreGeneral_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",0) # 0 = General
-def cbxOnlyCountGeneral_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",0)
-
-def cbxIgnoreChampion_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",1) # 1 = Champion
-def cbxOnlyCountChampion_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",1)
-
-def cbxIgnoreGiant_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",4) # 4 = Giant
-def cbxOnlyCountGiant_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",4)
-
-def cbxIgnoreTitan_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",5) # 5 = Titan
-def cbxOnlyCountTitan_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",5)
-
-def cbxIgnoreStrong_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",6) # 6 = Strong
-def cbxOnlyCountStrong_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",6)
-
-def cbxIgnoreElite_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",7) # 7 = Elite
-def cbxOnlyCountElite_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",7)
-
-def cbxIgnoreUnique_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",8) # 8 = Unique
-def cbxOnlyCountUnique_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",8)
-
-def cbxIgnoreParty_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",16) # 16 = Party
-def cbxOnlyCountParty_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",16)
-
-def cbxIgnoreChampionParty_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",17) # 17 = ChampionParty
-def cbxOnlyCountChampionParty_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",17)
-
-def cbxIgnoreGiantParty_clicked(checked):
-	Checkbox_Checked(checked,"lstIgnore",20) # 20 = GiantParty
-def cbxOnlyCountGiantParty_clicked(checked):
-	Checkbox_Checked(checked,"lstOnlyCount",20)
-
-def cbxAcceptForgottenWorld_checked(checked):
-	saveConfigs()
-
-# Generalizing checkbox methods
-def Checkbox_Checked(checked,gListName,mobType):
-	gListReference = globals()[gListName]
-	if checked:
-		gListReference.append(mobType)
-	else:
-		gListReference.remove(mobType)
-	saveConfigs()
-
-# Return folder path
-def getPath():
-	return get_config_dir()+pName+"\\"
-
-# Return character configs path (JSON)
-def getConfig():
-	return getPath()+character_data['server'] + "_" + character_data['name'] + ".json"
-
-# Load default configs
-def loadDefaultConfig():
-	# Clear data
-	global lstMobsData,lstIgnore,lstOnlyCount
-
-	lstMobsData = []
-	QtBind.clear(gui,lstMobs)
-
-	lstIgnore = []
-	QtBind.setChecked(gui,cbxIgnoreGeneral,False)
-	QtBind.setChecked(gui,cbxIgnoreChampion,False)
-	QtBind.setChecked(gui,cbxIgnoreGiant,False)
-	QtBind.setChecked(gui,cbxIgnoreTitan,False)
-	QtBind.setChecked(gui,cbxIgnoreStrong,False)
-	QtBind.setChecked(gui,cbxIgnoreElite,False)
-	QtBind.setChecked(gui,cbxIgnoreUnique,False)
-	QtBind.setChecked(gui,cbxIgnoreParty,False)
-	QtBind.setChecked(gui,cbxIgnoreChampionParty,False)
-	QtBind.setChecked(gui,cbxIgnoreGiantParty,False)
-
-	lstOnlyCount = []
-	QtBind.setChecked(gui,cbxOnlyCountGeneral,False)
-	QtBind.setChecked(gui,cbxOnlyCountChampion,False)
-	QtBind.setChecked(gui,cbxOnlyCountGiant,False)
-	QtBind.setChecked(gui,cbxOnlyCountTitan,False)
-	QtBind.setChecked(gui,cbxOnlyCountStrong,False)
-	QtBind.setChecked(gui,cbxOnlyCountElite,False)
-	QtBind.setChecked(gui,cbxOnlyCountUnique,False)
-	QtBind.setChecked(gui,cbxOnlyCountParty,False)
-	QtBind.setChecked(gui,cbxOnlyCountChampionParty,False)
-	QtBind.setChecked(gui,cbxOnlyCountGiantParty,False)
-
-	QtBind.setChecked(gui,cbxAcceptForgottenWorld,False)
-
-# Load config if exists
-def loadConfigs():
-	loadDefaultConfig()
-	if isJoined() and os.path.exists(getConfig()):
-		data = {}
-		with open(getConfig(),"r") as f:
-			data = json.load(f)
-		# Check to load config
-		if "Ignore Names" in data:
-			global lstMobsData
-			lstMobsData = data["Ignore Names"]
-			# Load names visually
-			for name in lstMobsData:
-				QtBind.append(gui,lstMobs,name)
-		
-		if "Ignore Types" in data:
-			global lstIgnore
-			for t in data["Ignore Types"]:
-				if t == 8:
-					QtBind.setChecked(gui,cbxIgnoreUnique,True)
-				elif t == 7:
-					QtBind.setChecked(gui,cbxIgnoreElite,True)
-				elif t == 6:
-					QtBind.setChecked(gui,cbxIgnoreStrong,True)
-				elif t == 5:
-					QtBind.setChecked(gui,cbxIgnoreTitan,True)
-				elif t == 4:
-					QtBind.setChecked(gui,cbxIgnoreGiant,True)
-				elif t == 1:
-					QtBind.setChecked(gui,cbxIgnoreChampion,True)
-				elif t == 0:
-					QtBind.setChecked(gui,cbxIgnoreGeneral,True)
-				elif t == 16:
-					QtBind.setChecked(gui,cbxIgnoreParty,True)
-				elif t == 17:
-					QtBind.setChecked(gui,cbxIgnoreChampionParty,True)
-				elif t == 20:
-					QtBind.setChecked(gui,cbxIgnoreGiantParty,True)
-				else:
-					# skip it if is not filtered
-					continue
-				lstIgnore.append(t)
-
-		if "OnlyCount Types" in data:
-			global lstOnlyCount
-			for t in data["OnlyCount Types"]:
-				if t == 8:
-					QtBind.setChecked(gui,cbxOnlyCountUnique,True)
-				elif t == 7:
-					QtBind.setChecked(gui,cbxOnlyCountElite,True)
-				elif t == 6:
-					QtBind.setChecked(gui,cbxOnlyCountStrong,True)
-				elif t == 5:
-					QtBind.setChecked(gui,cbxOnlyCountTitan,True)
-				elif t == 4:
-					QtBind.setChecked(gui,cbxOnlyCountGiant,True)
-				elif t == 1:
-					QtBind.setChecked(gui,cbxOnlyCountChampion,True)
-				elif t == 0:
-					QtBind.setChecked(gui,cbxOnlyCountGeneral,True)
-				elif t == 16:
-					QtBind.setChecked(gui,cbxOnlyCountParty,True)
-				elif t == 17:
-					QtBind.setChecked(gui,cbxOnlyCountChampionParty,True)
-				elif t == 20:
-					QtBind.setChecked(gui,cbxOnlyCountGiantParty,True)
-				else:
-					# skip it if is not filtered
-					continue
-				lstOnlyCount.append(t)
-
-		if 'Accept ForgottenWorld' in data and data['Accept ForgottenWorld']:
-			QtBind.setChecked(gui,cbxAcceptForgottenWorld,True)
-
-# Save all config
-def saveConfigs():
-	# Save if data has been loaded
-	if isJoined():
-		# Save all data
-		data = {}
-
-		data['OnlyCount Types'] = lstOnlyCount
-		data['Ignore Types'] = lstIgnore
-		data['Ignore Names'] = lstMobsData
-		data['Accept ForgottenWorld'] = QtBind.isChecked(gui,cbxAcceptForgottenWorld)
-
-		# Overrides
-		with open(getConfig(),"w") as f:
-			f.write(json.dumps(data, indent=4, sort_keys=True))
-
-# Check if character is ingame
-def isJoined():
-	global character_data
-	character_data = get_character_data()
-	if not (character_data and "name" in character_data and character_data["name"]):
-		character_data = None
-	return character_data
-
-# Add mob to the list
-def btnAddMob_clicked():
-	global lstMobsData
-	# Check name
-	text = QtBind.text(gui,tbxMobs)
-	if text and not ListContains(text,lstMobsData):
-		lstMobsData.append(text)
-		# Add visually
-		QtBind.append(gui,lstMobs,text)
-		QtBind.setText(gui,tbxMobs,"")
-		saveConfigs()
-		log('Plugin: Monster added ['+text+']')
-
-# Add mob to the list
-def btnRemMob_clicked():
-	global lstMobsData
-	# Check selected
-	selected = QtBind.text(gui,lstMobs)
-	if selected:
-		lstMobsData.remove(selected)
-		# Remove visually
-		QtBind.remove(gui,lstMobs,selected)
-		saveConfigs()
-		log('Plugin: Monster removed ['+selected+']')
-
-# Return True if text exist at the list
-def ListContains(text,lst):
-	text = text.lower()
-	for i in range(len(lst)):
-		if lst[i].lower() == text:
-			return True
-	return False
-
-# Return True if item exists
-def QtBind_ItemsContains(text,lst):
-	return ListContains(text,QtBind.getItems(gui,lst))
+gui = QtBind.init(__name__, pName)
 
 # Calc the distance from point A to B
-def GetDistance(ax,ay,bx,by):
-	return ((bx-ax)**2 + (by-ay)**2)**(0.5)
+def GetDistance(ax, ay, bx, by):
+	return ((bx - ax) ** 2 + (by - ay) ** 2) ** (0.5)
 
 # Returns the item information if is found
 def GetDimensionalHole(Name):
@@ -368,34 +60,34 @@ def EnterToDimensional(Name):
 	if uid:
 		# Select dimensional
 		log('Plugin: Selecting dimensional hole...')
-		packet = struct.pack('I',uid)
-		inject_joymax(0x7045,packet,False)
+		packet = struct.pack('I', uid)
+		inject_joymax(0x7045, packet, False)
 		sleep(1.0)
 		# Enter and select the option
 		log('Plugin: Entering to dimensional hole...')
-		inject_joymax(0x704B,packet,False)
-		packet += struct.pack('H',3)
-		inject_joymax(0x705A,packet,False)
+		inject_joymax(0x704B, packet, False)
+		packet += struct.pack('H', 3)
+		inject_joymax(0x705A, packet, False)
 		# Start bot, doesn't matter if is teleporting
-		Timer(5.0,start_bot).start()
+		Timer(5.0, start_bot).start()
 		return
 	# Error message
-	log('Plugin: "'+Name+'" cannot be found around you!')
+	log('Plugin: "' + Name + '" cannot be found around you!')
 
 # Avoid interpreter lock
 def GoDimensionalThread(Name):
 	# Check if dimensional still opened
 	if dimensionalItemActivated:
 		Name = dimensionalItemActivated['name']
-		log('Plugin: '+( '"'+Name+'"' if Name else 'Dimensional Hole')+' still opened!')
+		log('Plugin: ' + ('"' + Name + '"' if Name else 'Dimensional Hole') + ' still opened!')
 		EnterToDimensional(Name)
 		return
 	# Check if the item exists on inventory
 	item = GetDimensionalHole(Name)
 	if item:
 		# Inject item usage
-		log('Plugin: Using "'+item['name']+'"...')
-		p = struct.pack('B',item['slot'])
+		log('Plugin: Using "' + item['name'] + '"...')
+		p = struct.pack('B', item['slot'])
 		locale = get_locale()
 
 		if locale in [56, 18, 61]: # TRSRO, iSRO, and VTC
@@ -405,12 +97,10 @@ def GoDimensionalThread(Name):
 		# Set item used
 		global itemUsedByPlugin
 		itemUsedByPlugin = item
-		inject_joymax(0x704C,p,True)
+		inject_joymax(0x704C, p, True)
 	else:
 		# Error message
-		log('Plugin: '+( '"'+Name+'"' if Name else 'Dimensional Hole')+' cannot be found at your inventory')
-
-# ______________________________ Events ______________________________ #
+		log('Plugin: ' + ('"' + Name + '"' if Name else 'Dimensional Hole') + ' cannot be found at your inventory')
 
 # Use, select and enter to the dimensional forgotten world. 
 # Ex: "GoDimensional" or "GoDimensional,Dimension Hole (Flame Mountain-3 stars)"
@@ -422,50 +112,34 @@ def GoDimensional(args):
 	if len(args) > 1:
 		name = args[1]
 	# Avoid lock
-	Timer(0.001,GoDimensionalThread,[name]).start()
+	Timer(0.001, GoDimensionalThread, [name]).start()
 	return 0
-
-# Called when the character enters the game world
-def joined_game():
-	loadConfigs()
 
 # All packets received from game server will be passed to this function
 # Returning True will keep the packet and False will not forward it to the game client
 def handle_joymax(opcode, data):
 	# SERVER_INVENTORY_ITEM_USE
-	elif opcode == 0xB04C:
+	if opcode == 0xB04C:
 		# Just check recent item used to keep it simple
 		global itemUsedByPlugin
 		if itemUsedByPlugin:
 			# Success
 			if data[0] == 1:
-				log('Plugin: "'+itemUsedByPlugin['name']+'" has been opened')
+				log('Plugin: "' + itemUsedByPlugin['name'] + '" has been opened')
 				# Set timer for cooldown usage
 				global dimensionalItemActivated
 				dimensionalItemActivated = itemUsedByPlugin
 				def DimensionalCooldown():
 					global dimensionalItemActivated
 					dimensionalItemActivated = None
-				Timer(DIMENSIONAL_COOLDOWN_DELAY,DimensionalCooldown).start()
+				Timer(DIMENSIONAL_COOLDOWN_DELAY, DimensionalCooldown).start()
 				# Avoid locking the proxy thread
-				Timer(1.0,EnterToDimensional,[itemUsedByPlugin['name']]).start()
+				Timer(1.0, EnterToDimensional, [itemUsedByPlugin['name']]).start()
 			else:
-				log('Plugin: "'+itemUsedByPlugin['name']+'" cannot be opened')
+				log('Plugin: "' + itemUsedByPlugin['name'] + '" cannot be opened')
 			# Stop checking item used
 			itemUsedByPlugin = None
 	return True
 
 # Plugin loaded
-log('Plugin: '+pName+' v'+pVersion+' succesfully loaded')
-
-if os.path.exists(getPath()):
-	# Adding RELOAD plugin support
-	try:
-		loadConfigs()
-	except:
-		# Just in case omg -_-
-		log('Plugin: Error loading '+pName+' config file')
-else:
-	# Creating configs folder
-	os.makedirs(getPath())
-	log('Plugin: '+pName+' folder has been created')
+log('Plugin: ' + pName + ' v' + pVersion + ' succesfully loaded')
